ec79222ae25353eb867cd93deb66e25419147bd7
Arnald da Costa Queiroga <CIANDT\arnald@lnb036081spo.ciandt.global>
2022-06-13 15:49:00 -0300

    Enviando correção

--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -1,6 +1,6 @@
-public class PrintFloat {
+public class PrintStringVariable {
     public static void main(String[] args) {
-        String texto = "Holberton School" ;
-        System.out.println("\n" + texto + "\n" + texto + "\n" + texto.substring(0,9));
+        String texto = "Holberton School";
+        System.out.printf(texto +"\n"+texto+"\n"+texto.substring(0,9)+"\n");
     }
 }
