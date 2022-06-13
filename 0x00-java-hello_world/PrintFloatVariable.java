669a9ae477eafb28634f05b972b075ed6fae1bfe
Arnald <107144434+arnald-queiroga@users.noreply.github.com>
2022-06-13 15:35:17 -0300

    Update PrintFloatVariable.java

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,4 +1,4 @@
-public class PrintFloat {
+public class PrintStringVariable {
     public static void main(String[] args) {
         String texto = "Holberton School";
         System.out.println(texto+"\n"+texto+"\n"+texto.substring(0,9)+"\n");
