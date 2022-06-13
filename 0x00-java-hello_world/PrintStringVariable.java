626e441ec50417f765bdf88fda2ffa94b6e18cc0
Arnald da Costa Queiroga <CIANDT\arnald@lnb036081spo.ciandt.global>
2022-06-13 15:18:13 -0300

    3. Print String Variable

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,6 @@
+public class PrintFloat {
+    public static void main(String[] args) {
+        String texto = "Holberton School" ;
+        System.out.println("\n" + texto + "\n" + texto + "\n" + texto.substring(0,9));
+    }
+}
