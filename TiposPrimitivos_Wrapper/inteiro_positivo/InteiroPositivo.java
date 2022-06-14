03675fa824132a534f8b901591785ceffa88e83e
Arnald da Costa Queiroga <CIANDT\arnald@lnb036081spo.ciandt.global>
2022-06-14 14:51:43 -0300

    Alterando o conteúdo do arquivo InteiroPositivo

--- TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -1,54 +1,11 @@
 public class InteiroPositivo {
     public static void main(String[] args) {
-        try {
-            InteiroPositivo inteiroPositivo = new InteiroPositivo("1");
-            System.out.println(inteiroPositivo.getValor() + " - " + inteiroPositivo.ehPrimo());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
 
-        InteiroPositivo inteiroPositivo1 = null;
+        Integer valor = new Integer(10);
 
-        try {
-            inteiroPositivo1 = new InteiroPositivo("43");
-            System.out.println(inteiroPositivo1.getValor() + " - " + inteiroPositivo1.ehPrimo());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        int num1 = valor.intValue();
 
-        try {
-            inteiroPositivo1.setValor(-1);
-            System.out.println(inteiroPositivo1.getValor() + " - " + inteiroPositivo1.ehPrimo());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        System.out.println(num1);
 
-        try {
-            inteiroPositivo1.setValor(inteiroPositivo1.getValor() + 1);
-            System.out.println(inteiroPositivo1.getValor() + " - " + inteiroPositivo1.ehPrimo());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        try {
-            InteiroPositivo inteiroPositivo = new InteiroPositivo("-3");
-            System.out.println(inteiroPositivo.getValor() + " - " + inteiroPositivo.ehPrimo());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        try {
-            InteiroPositivo inteiroPositivo = new InteiroPositivo(379);
-            System.out.println(inteiroPositivo.getValor() + " - " + inteiroPositivo.ehPrimo());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        try {
-            InteiroPositivo inteiroPositivo = new InteiroPositivo(378);
-            System.out.println(inteiroPositivo.getValor() + " - " + inteiroPositivo.ehPrimo());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
     }
 }
