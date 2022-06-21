d1d011db000e98c9d1ff8af13aff5a922bfc28fb
Arnald da Costa Queiroga <CIANDT\arnald@lnb036081spo.ciandt.global>
2022-06-21 11:23:29 -0300

    0. Numeros 0 a 99

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,13 @@
+public class Numeros0a99 {
+    public static void main(String[] args) {
+        int num = 0;
+        while (num < 99) {
+            if (num < 98 ) {
+                System.out.print(num + 1 + ", ");
+            } else {
+                System.out.print(num + 1 + " " + "\n");
+            }
+            num++;
+        }
+    }
+}
