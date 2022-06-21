9497d01d3ca210ba0dd3b1e61c446ae82795c43e
Arnald da Costa Queiroga <CIANDT\arnald@lnb036081spo.ciandt.global>
2022-06-21 13:51:25 -0300

    2. Letra Maiúscula

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,9 @@
+public class Caractere {
+    public static boolean EhMaiusculo(char c){
+        if(c > 65 && c <=91){
+            return true;
+        } else {
+            return false;
+        }
+    }
+} 
