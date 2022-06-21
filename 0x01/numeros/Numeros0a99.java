92f9bcf4cb3446eb08a6dcb97f0bbd7184b1eb51
Arnald <107144434+arnald-queiroga@users.noreply.github.com>
2022-06-21 11:47:15 -0300

    Update Numeros0a99.java

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -1,7 +1,7 @@
 public class Numeros0a99 {
     public static void main(String[] args) {
         for(int i = 0; i <= 99; i++){
-            if(i < 98){
+            if(i < 99){
                 System.out.printf(i + ", ");
             } else {
                 System.out.printf(i + "\n");
