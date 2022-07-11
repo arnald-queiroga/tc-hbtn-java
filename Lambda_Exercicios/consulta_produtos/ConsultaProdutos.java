de62c47bc40f59773409806564a0c77c883c110d
Arnald da Costa Queiroga <CIANDT\arnald@lnb036081spo.ciandt.global>
2022-07-11 16:42:48 -0300

    Task 0

---
+++ Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsultaProdutos {
+
+    public static List<Produto> filtrar(List<Produto> produtos, CriterioFiltro criterioFiltro) {
+        ArrayList<Produto> lista = new ArrayList<>();
+
+        produtos.forEach(produto -> {
+            if (criterioFiltro.testar(produto)){
+                lista.add(produto);
+            }
+        });
+
+        return lista;
+    }
+} 
+
---
+++ Lambda_Exercicios/consulta_produtos/CriterioFiltro.java
@@ -0,0 +1,6 @@
+import java.util.List;
+
+public interface CriterioFiltro {
+    boolean testar(Produto produto);
+} 
+
