22fa5f27f0513640a553e237424a963c280de8dc
Arnald <107144434+arnald-queiroga@users.noreply.github.com>
2022-07-14 15:35:34 -0300

    Update Estudante.java

--- Serializacao/serializacao/Estudante.java
+++ Serializacao/serializacao/Estudante.java
@@ -36,6 +36,6 @@ public class Estudante implements Serializable {
 
     @Override
     public String toString() {
-        return String.format("Estudando { nome='%s', idade='%d', senha'%s' }", nome, idade, senha);
+        return String.format("Estudante { nome='%s', idade='%d', senha='%s' }", nome, idade, senha);
     }
 } 
